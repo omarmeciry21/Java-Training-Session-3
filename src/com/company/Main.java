80f8b8029025f96613a4835cb5096543afe41dc3
Omar <omarmeciry21@gmail.com>
2022-03-09 23:31:46 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+//    public static void printDetails(String name, String email,boolean isAdmin, int age){
+//        System.out.println("=======================================");
+//        System.out.println("Name: "+name);
+//        System.out.println("Email: "+email);
+//        System.out.println("Is Admin?: "+isAdmin);
+//        System.out.println("Age: "+age);
+//        System.out.println("=======================================");
+//    }
+
+//    public static boolean isEven(int number){
+//
+//        return number%2==0;
+//    }
+
+//    public static double findMaxNumber(int n1,int n2,int n3){
+//        if(n1>=n2&&n1>=n3){
+//            return n1;
+//        }
+//        else if(n2>=n1&&n2>=n3){
+//            return n2;
+//        }
+//        else{
+//            return n3;
+//        }
+//    }
+
+    public static void main(String[] args) {
+//	    System.out.println("Enter first number: ");
+//        Scanner sc = new Scanner(System.in);
+//        int n1 = sc.nextInt();
+//        System.out.println("Enter second number: ");
+//        int n2 = sc.nextInt();
+//        int sum = sum(n1,n2);
+//        System.out.println("Sum = "+sum);
+
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("Name: ");
+//        String userName = sc.nextLine();
+//        System.out.print("Email: ");
+//        String userEmail = sc.nextLine();
+//        System.out.print("Is Admin?: ");
+//        boolean isUserAdmin = sc.nextBoolean();
+//        System.out.print("Age: ");
+//        int userAge = sc.nextInt();
+//        printDetails(userName,userEmail,isUserAdmin,userAge);
+
+//        System.out.println("Enter first number: ");
+//        Scanner sc =new Scanner(System.in);
+//        int n1 = sc.nextInt();
+
+//        System.out.println("Enter second number: ");
+//        int n2 = sc.nextInt();
+//        System.out.println("Enter third number: ");
+//        int n3 = sc.nextInt();
+//        System.out.println(findMaxNumber(n1,n2,n3));
+    }
+}
